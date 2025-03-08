88aa187a3c88d6befad6b00cdd905fe2988e25a5
Aaditya Jauhari <aadityajauhari01@gmail.com>
2025-03-08 06:01:10 +0530

    Update homepage.js
    Added api

--- zenins/Firebase/homepage.js
+++ zenins/Firebase/homepage.js
@@ -3,10 +3,19 @@ import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/fire
 import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
 
 const firebaseConfig = {
-    //YOUR COPIED FIREBASE PART SHOULD BE HERE
- //WATCH THIS VIDEO TO LEARN WHAT TO PUT HERE   https://youtu.be/_Xczf06n6x0
+    apiKey: "AIzaSyDJ_AEd8v-nPtTT_h20iNaxemV44b5cz9c",
+    authDomain: "strot-9656b.firebaseapp.com",
+    projectId: "strot-9656b",
+    storageBucket: "strot-9656b.firebasestorage.app",
+    messagingSenderId: "647548665754",
+    appId: "1:647548665754:web:4b8b626b34c2b76f3e4423",
+    measurementId: "G-3TNX3B717X"
   };
- 
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
